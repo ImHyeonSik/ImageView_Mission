e2609c741ac714e4d44ac79ba38c714d105f1e38
hyeonsik <dgt4862@gmail.com>
2022-04-30 20:57:59 +0900

    imageView를 활용한 옆으로 갤러리 넘어가게 함

--- ImageView_Mission/ViewController.swift
+++ ImageView_Mission/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var numImage = 1
+    var maxImage = 3
 
+    @IBOutlet var imgViewer: UIImageView!
+    
+    @IBOutlet var BtnBack: UIButton!
+    
+    @IBOutlet var BtnNext: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imgViewer.image = UIImage(named: "1.png")
+        
+        
     }
+    
+    @IBAction func BackImage(_ sender: UIButton) {
+        numImage = numImage - 1
+        if(numImage < 1) {
+            numImage = maxImage
+        }
+        
+        let imageName = String(numImage) + ".png"
+        imgViewer.image = UIImage(named: imageName)
+    }
+    
 
-
+    @IBAction func NextImage(_ sender: UIButton) {
+        numImage = numImage + 1
+        if(numImage > maxImage){
+            numImage = 1
+        }
+        
+        let imageName = String(numImage) + ".png"
+        imgViewer.image = UIImage(named: imageName)
+    }
+    
 }
 
